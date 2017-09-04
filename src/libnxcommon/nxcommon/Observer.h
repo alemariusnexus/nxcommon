494571ec04a370a2fd26adc321f48674013734ea
Alemarius Nexus <alemariusnexus@gmail.com>
2017-09-04 10:05:53 +0200

    Macros for supporting Observer class through SWIG.

--- src/libnxcommon/nxcommon/Observer.h
+++ src/libnxcommon/nxcommon/Observer.h
@@ -8,6 +8,8 @@
 #include <mutex>
 
 
+#ifndef SWIG
+
 template <typename... ArgTypes>
 class Observer;
 
@@ -127,5 +129,63 @@ private:
 	ObservableType* obsv;
 	CallbackType cb;
 };
+
+#endif
+
+
+
+
+
+
+#define DefineSwigObserver_n_m(clsname, methodname, obsvcls, cargs, cargns, args, pargs) \
+	class clsname \
+	{ \
+	\
+	public: \
+		clsname(cargs) \
+				: obsv(cargns std::bind(&clsname::methodname, this pargs)) \
+		{} \
+		virtual ~clsname() {} \
+		\
+		virtual void methodname(args) = 0; \
+		\
+	private: \
+		obsvcls obsv; \
+	};
+
+
+#pragma SWIG nowarn=305
+
+#define _COMMA ,
+
+#pragma SWIG nowarn=
+
+
+#define DefineSwigObserver_0_0(clsname, methodname, obsvcls) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, , , , )
+#define DefineSwigObserver_0_1(clsname, methodname, obsvcls, arg1) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, , , arg1, _COMMA std::placeholders::_1)
+#define DefineSwigObserver_0_2(clsname, methodname, obsvcls, arg1, arg2) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, , , arg1 _COMMA arg2, _COMMA std::placeholders::_1 _COMMA std::placeholders::_2)
+#define DefineSwigObserver_0_3(clsname, methodname, obsvcls, arg1, arg2, arg3) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, , , arg1 _COMMA arg2 _COMMA arg3, _COMMA std::placeholders::_1 _COMMA std::placeholders::_2 _COMMA std::placeholders::_3)
+#define DefineSwigObserver_0_4(clsname, methodname, obsvcls, arg1, arg2, arg3, arg4) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, , , arg1 _COMMA arg2 _COMMA arg3 _COMMA arg4, _COMMA std::placeholders::_1 _COMMA std::placeholders::_2 _COMMA std::placeholders::_3 _COMMA std::placeholders::_4)
+#define DefineSwigObserver_0_5(clsname, methodname, obsvcls, arg1, arg2, arg3, arg4, arg5) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, , , arg1 _COMMA arg2 _COMMA arg3 _COMMA arg4 _COMMA arg5, _COMMA std::placeholders::_1 _COMMA std::placeholders::_2 _COMMA std::placeholders::_3 _COMMA std::placeholders::_4 _COMMA std::placeholders::_5)
+
+
+#define DefineSwigObserver_1_0(clsname, methodname, obsvcls, carg1) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, carg1 a1, a1 _COMMA, , )
+#define DefineSwigObserver_1_1(clsname, methodname, obsvcls, carg1, arg1) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, carg1 a1, a1 _COMMA, arg1, _COMMA std::placeholders::_1)
+#define DefineSwigObserver_1_2(clsname, methodname, obsvcls, carg1, arg1, arg2) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, carg1 a1, a1 _COMMA, arg1 _COMMA arg2, _COMMA std::placeholders::_1 _COMMA std::placeholders::_2)
+#define DefineSwigObserver_1_3(clsname, methodname, obsvcls, carg1, arg1, arg2, arg3) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, carg1 a1, a1 _COMMA, arg1 _COMMA arg2 _COMMA arg3, _COMMA std::placeholders::_1 _COMMA std::placeholders::_2 _COMMA std::placeholders::_3)
+#define DefineSwigObserver_1_4(clsname, methodname, obsvcls, carg1, arg1, arg2, arg3, arg4) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, carg1 a1, a1 _COMMA, arg1 _COMMA arg2 _COMMA arg3 _COMMA arg4, _COMMA std::placeholders::_1 _COMMA std::placeholders::_2 _COMMA std::placeholders::_3 _COMMA std::placeholders::_4)
+#define DefineSwigObserver_1_5(clsname, methodname, obsvcls, carg1, arg1, arg2, arg3, arg4, arg5) \
+	DefineSwigObserver_n_m(clsname, methodname, obsvcls, carg1 a1, a1 _COMMA, arg1 _COMMA arg2 _COMMA arg3 _COMMA arg4 _COMMA arg5, _COMMA std::placeholders::_1 _COMMA std::placeholders::_2 _COMMA std::placeholders::_3 _COMMA std::placeholders::_4 _COMMA std::placeholders::_5)
 
 #endif /* NXCOMMON_SRC_LIBNXCOMMON_NXCOMMON_OBSERVER_H_ */
